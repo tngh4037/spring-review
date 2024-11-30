651dc22f76fce7bedc6dc5393939f0c5e19906b8
tngh4037 <tngh4037@gmail.com>
2024-12-01 05:44:03 +0900

    study: 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private final Map<Long, Member> members = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        members.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return members.get(id);
+    }
+}
