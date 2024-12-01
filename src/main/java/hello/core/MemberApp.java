b28259d335f06b5e6a35704a15724d30979b718e
tngh4037 <tngh4037@gmail.com>
2024-12-01 09:07:23 +0900

    study: 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+/*
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+ */
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
