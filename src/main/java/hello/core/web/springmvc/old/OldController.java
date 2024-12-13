6f0fd16b38534d9ac472357689cd85419eb40290
tngh4037 <tngh4037@gmail.com>
2024-12-14 01:59:52 +0900

    study: 스프링이 자동 등록하는 핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/hello/core/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.core.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") // 참고) 스프링 빈의 이름을 url 요청 경로로 사용
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
