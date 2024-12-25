4458e29080cb3061dc7cdc1686e789dd56519cd7
tngh4037 <tngh4037@gmail.com>
2024-12-25 15:48:57 +0900

    project init

--- src/main/java/hello/core/WebConfig.java
+++ src/main/java/hello/core/WebConfig.java
@@ -1,22 +1,8 @@
 package hello.core;
 
-import hello.core.filter.LogFilter;
-import jakarta.servlet.DispatcherType;
-import jakarta.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
 
-    // @Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new LogFilter());
-        filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("/*");
-        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
-        return filterFilterRegistrationBean;
-    }
 }
