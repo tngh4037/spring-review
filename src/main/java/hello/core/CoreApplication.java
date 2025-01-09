3b28688df388f097dc9297bc8cb9de23c5c48747
tngh4037 <tngh4037@gmail.com>
2025-01-10 06:28:10 +0900

    study: JdbcTemplate

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.config.JdbcTemplateV1Config;
+import hello.core.config.JdbcTemplateV2Config;
+import hello.core.config.JdbcTemplateV3Config;
 import hello.core.config.MemoryConfig;
 import hello.core.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Import(MemoryConfig.class)
+// @Import(MemoryConfig.class)
+// @Import(JdbcTemplateV1Config.class)
+// @Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.core.web")
 public class CoreApplication {
 
--- src/main/java/hello/core/service/ItemService.java
+++ src/main/java/hello/core/service/ItemService.java
@@ -7,6 +7,8 @@ import hello.core.repository.ItemUpdateDto;
 import java.util.List;
 import java.util.Optional;
 
+// 서비스 계층에 인터페이스를 도입하는 경우는 사실 그렇게 많지는 않다.
+//  ㄴ 인터페이스를 도입한다는 것: 미래에 구현체를 바꿀 가능성이 있을 때 도입한다.
 public interface ItemService {
 
     Item save(Item item);
