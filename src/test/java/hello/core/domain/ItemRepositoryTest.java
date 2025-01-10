2bf2c4d4a68a7ccdd4a9cdb2f236b01a4b350030
tngh4037 <tngh4037@gmail.com>
2025-01-11 02:50:39 +0900

    study: 테스트

--- src/test/java/hello/core/domain/ItemRepositoryTest.java
+++ src/test/java/hello/core/domain/ItemRepositoryTest.java
@@ -5,19 +5,38 @@ import hello.core.repository.ItemSearchCond;
 import hello.core.repository.ItemUpdateDto;
 import hello.core.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 public class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    /*
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+    */
 
     @AfterEach
     void afterEach() {
@@ -25,8 +44,12 @@ public class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        // 트랜잭션 롤백
+        // transactionManager.rollback(status);
     }
 
+    // @Commit
     @Test
     void save() {
         //given
