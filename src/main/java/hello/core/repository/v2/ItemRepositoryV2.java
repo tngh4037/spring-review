7e6c9e609cfd0383eedd82ea33f9710fb0a49902
tngh4037 <tngh4037@gmail.com>
2025-01-13 07:58:29 +0900

    project init

--- src/main/java/hello/core/repository/v2/ItemRepositoryV2.java
+++
@@ -1,7 +0,0 @@
-package hello.core.repository.v2;
-
-import hello.core.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepositoryV2 extends JpaRepository<Item, Long> {
-}
