be12245d1a60a7bd478736e91fdee51353560b0d
tngh4037 <tngh4037@gmail.com>
2025-01-23 02:03:21 +0900

    study: 템플릿 메서드 패턴

---
+++ src/main/java/hello/core/app/v4/OrderServiceV4.java
@@ -0,0 +1,25 @@
+package hello.core.app.v4;
+
+import hello.core.trace.logtrace.LogTrace;
+import hello.core.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
