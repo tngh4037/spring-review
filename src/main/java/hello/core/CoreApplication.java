8760404960db89879a13302a3f97cfeb706637a2
tngh4037 <tngh4037@gmail.com>
2025-01-25 05:35:54 +0900

    study: 구체 클래스 기반 프록시 적용

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.config.AppV1Config;
 import hello.core.config.AppV2Config;
+import hello.core.config.v1_proxy.ConcreteProxyConfig;
 import hello.core.config.v1_proxy.InterfaceProxyConfig;
 import hello.core.trace.logtrace.LogTrace;
 import hello.core.trace.logtrace.ThreadLocalLogTrace;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.core.app.v3")
 public class CoreApplication {
 
---
+++ src/main/java/hello/core/config/v1_proxy/concrete_proxy/OrderRepositoryConcreteProxy.java
@@ -0,0 +1,32 @@
+package hello.core.config.v1_proxy.concrete_proxy;
+
+import hello.core.app.v2.OrderRepositoryV2;
+import hello.core.trace.TraceStatus;
+import hello.core.trace.logtrace.LogTrace;
+
+public class OrderRepositoryConcreteProxy extends OrderRepositoryV2 {
+
+    private final OrderRepositoryV2 target;
+    private final LogTrace logTrace;
+
+    public OrderRepositoryConcreteProxy(OrderRepositoryV2 target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.request()");
+
+            // target 호출
+            target.save(itemId);
+
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
