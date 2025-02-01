6120f83e8a4ec4a94bd3b396862c310446411418
tngh4037 <tngh4037@gmail.com>
2025-02-01 23:09:29 +0900

    study: 프록시 팩토리 적용

--- src/test/java/hello/core/advisor/MultiAdvisorTest.java
+++ src/test/java/hello/core/advisor/MultiAdvisorTest.java
@@ -36,12 +36,12 @@ public class MultiAdvisorTest {
         // 실행
         proxy2.save();
     }
-    
+
     @Test
     @DisplayName("하나의 프록시, 여러 어드바이저")
     void multiAdvisorTest2() {
         // 상황: client -> proxy -> advisor2 -> advisor1 -> target
-        
+
         // proxy 생성
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target);
@@ -63,8 +63,10 @@ public class MultiAdvisorTest {
 
         @Override
         public Object invoke(MethodInvocation invocation) throws Throwable {
-            log.info("advice1 호출");
-            return invocation.proceed();
+            log.info("advice1 호출 시작");
+            Object result = invocation.proceed();
+            log.info("advice1 호출 종료");
+            return result;
         }
     }
 
@@ -73,8 +75,10 @@ public class MultiAdvisorTest {
 
         @Override
         public Object invoke(MethodInvocation invocation) throws Throwable {
-            log.info("advice2 호출");
-            return invocation.proceed();
+            log.info("advice2 호출 시작");
+            Object result = invocation.proceed();
+            log.info("advice2 호출 종료");
+            return result;
         }
     }
 
